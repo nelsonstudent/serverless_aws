6f59e78c7d704ce1761cce54db10d41d5e31a5aa
nelsonstudent <nelson.amazonas@gmail.com>
2021-08-19 14:06:18 -0300

    first commit

---
+++ src/fetchItem.js
@@ -0,0 +1,31 @@
+"use strict";
+const AWS = require("aws-sdk");
+
+const fetchItem = async (event) => {
+
+    const dynamoDB = AWS.DynamoDB.DocumentClient();
+    const {id} = event.pathParameters
+
+    let item;
+
+    try {
+        const result = await dynamoDB.get({
+           TableName: "ItemTable",
+           key: {id} 
+        }).promise();
+
+        item = result.item;
+
+    } catch (error) {
+        console.log(error)
+    }
+
+    return {
+        statusCode: 200,
+        body: JSON.stringify(item),
+    };  
+};
+
+module.exports ={
+    handler: fetchItem,
+};
